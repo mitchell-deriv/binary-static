0673aa585f896d31aa89be23f37a688cc5833453
mitchell-deriv <mitchell@deriv.com>
2021-04-15 16:00:34 +0200

    styled all choose new virtual account type

--- src/templates/app/new_account/choose_account.jsx
+++ src/templates/app/new_account/choose_account.jsx
@@ -1,51 +1,20 @@
 import React from 'react';
 import { SeparatorLine } from '../../_common/components/separator_line.jsx';
 
-
 const ChooseAccount = () => {
 
-
-    const init = () => {
-        editable_fields   = {};
-        get_settings_data = {};
-        is_virtual        = Client.get('is_virtual');
-        residence         = Client.get('residence');
-        mt_acct_type      = getHashValue('mt5_redirect');
-        validations       = [];
-        // demo and synthetic mt accounts do not require tax info
-        is_mt_tax_required = /real/.test(mt_acct_type) && mt_acct_type.split('_').length > 2 && +State.getResponse('landing_company.config.tax_details_required') === 1;
-    };
     const options = [
         {
-            icon_list    : ['images/pages/welcome/cfd.svg'],
-            title        : it.L('CFD'),
-            desc         : it.L('Trade with leverage and low spreads for better returns on successful trades.'),
-            platform_list: [
-                {
-                    icon       : 'metatrader5',
-                    title      : it.L('MetaTrader 5'),
-                    description: it.L('Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.'),
-                },
-            ],
-            action_title: it.L('Trade on MetaTrader 5'),
-            action_id   : 'mt5',
+            icon_list   : ['images/pages/welcome/synthetics.svg'],
+            title       : it.L('Synthetics'),
+            desc        : it.L('Get Trading with Synthetics - the simulated market that\'s always open.'),
+            action_title: it.L('Get started'),
+            action_id   : 'default',
         },
         {
-            icon_list    : ['images/pages/welcome/options.svg', 'images/pages/welcome/multipliers.svg'],
-            title        : it.L('Options and multipliers'),
-            desc         : it.L('Earn fixed payouts with options, or trade multipliers to amplify your gains with limited risk.'),
-            platform_list: [
-                {
-                    icon       : 'metatrader5',
-                    title      : it.L('SmartTrader'),
-                    description: it.L('Trade options with Binary.com\'s legacy trading app.'),
-                },
-                {
-                    icon       : 'binarybot',
-                    title      : it.L('Binary Bot'),
-                    description: it.L('Trade options with Binary.com\'s legacy trading app.'),
-                },
-            ],
+            icon_list   : ['images/pages/welcome/financial.svg'],
+            title       : it.L('Financial'),
+            desc        : it.L('Get trading forex, commodities, and cryptocurrencies'),
             action_title: it.L('Get started'),
             action_id   : 'default',
         },
@@ -61,20 +30,17 @@ const ChooseAccount = () => {
                 {options.map(option => (<RenderOption key={option.title} option={option} />))}
             </div>
 
+            <SeparatorLine className='gr-padding-5' invisible />
+
+            <div className='choose-account-footer'>
+                <span>{it.L('You can switch between synthetics and financial at any time.')}</span>
+            </div>
+
             <SeparatorLine className='gr-padding-10' invisible />
+
         </div>
     );
 };
-
-const Platform = ({ title, description, icon }) => (
-    <div className='choose-account-platform-list-container'>
-        <img src={it.url_for(`images/pages/welcome/${icon}.svg`)} />
-        <div className='choose-account-platform'>
-            <p className='choose-account-platform-title'>{title}</p>
-            <p className='choose-account-platform-description'>{description}</p>
-        </div>
-    </div>
-);
 
 const RenderOption = ({ option }) => (
     <div className='gr-6 gr-12-p gr-12-m gr-parent'>
@@ -87,19 +53,11 @@ const RenderOption = ({ option }) => (
             <p id='upgrade_text' className='choose-account-title'>{option.title}</p>
             <p >{option.desc}</p>
 
-            <SeparatorLine className='gr-padding-5' />
-
-            <div className='choose-account-platform-container'>
-                <p id='upgrade_text' className='choose-account-title'>{it.L('Platforms')}</p>
-                {option.platform_list.map((platform) =>
-                    <Platform key={platform.title} {...platform} />
-                )}
-                <div className='choose-account-action-container'>
-                    <a id={option.action_id} className='button-secondary' href='javascript:;'>
-                        <span className='choose-account-action'>{option.action_title}</span>
-                    </a>
-                </div>
-            </div>
+            <SeparatorLine className='gr-padding-15' invisible />
+            
+            <a id={option.action_id} className='button-secondary' href='javascript:;'>
+                <span className='choose-account-action'>{option.action_title}</span>
+            </a>
         </div>
     </div>
 
